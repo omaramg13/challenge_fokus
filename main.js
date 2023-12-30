1618642bf0a2bbbdb1b9b16b7461356ecb13891f
omaramg13 <omaramg13@gmail.com>
2023-12-29 23:12:54 -0600

    Se modifica index.html, se reemplazan tags div por tags section, se reemplaza tags ul li por tags div y p, se agregan ids a elementos de tabs, boton y contador. Se modifica style.css, se reemplazan clases por ids que fueron agregados. Se modifica main.js, se agregan variables para temporizadores y elementos html.

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Hello, World!");
+const boton_principal = document.querySelectorAll(".boton boton_principal");
+const tabs = document.querySelectorAll(".tab");
+const contador = document.querySelectorAll(".contador");
+let contador_focus = 1500;
+let contador_dc = 300;
+let contador_dl = 90;
+
